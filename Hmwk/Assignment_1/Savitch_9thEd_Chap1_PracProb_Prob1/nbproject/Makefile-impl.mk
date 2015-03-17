00fd6d9fb0a4f148c3fe8d9f446c7dadc9d66433
ds2316476 <salazard@hotmail.com>
2015-03-16 19:35:01 -0700

    lab is closing
    This is cool, I'm on problem 6. the lab closed though so i'll have to continue at the starbucks

--- Hmwk/Assignment_1/Savitch_9thEd_Chap1_PracProb_Prob1/nbproject/Makefile-impl.mk
+++ Hmwk/Assignment_1/Savitch_9thEd_Chap1_PracProb_Prob1/nbproject/Makefile-impl.mk
@@ -24,7 +24,7 @@ CLEAN_SUBPROJECTS=${CLEAN_SUBPROJECTS_${SUBPROJECTS}}
 
 
 # Project Name
-PROJECTNAME=Savitch_9thEd_Chap1_PracProb1
+PROJECTNAME=Savitch_9thEd_Chap1_PracProb_Prob1
 
 # Active Configuration
 DEFAULTCONF=Debug
---
+++ Hmwk/Assignment_1/Savitch_9thEd_Chap1_ProgProj_Prob1/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: rcc
+ *
+ * Created on March 16, 2015, 7:16 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
